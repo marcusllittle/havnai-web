c2d8a304fffaa1250263b98590864ec0af2e687b
Marcus Little <marcusllittle@users.noreply.github.com>
2025-12-06 15:06:27 -0500

    Use generator page as root

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+// Temporary: make the site root use the same generator
+// experience as /test so joinhavn.io does not 404.
+// We can later replace this with a dedicated landing page
+// component that mirrors the original static index.html.
+export { default } from "./test";
+
